7ada45f2602c981f88eab404fad5ded1197be800
mahmadali3180 <mahmadali3180@gmail.com>
2023-05-04 17:19:14 +0500

    sldieInLeft Animation

--- src/components/HomePage/LandingPage.jsx
+++ src/components/HomePage/LandingPage.jsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
   return (
     <div id="home" className="width-100 landing_page_main_back_image">
       <div className="res-1440">
-        <h1 className="landing_page_main_back_image_div_cnt_h1">
+        <h1 className="sldieInLeft bounce landing_page_main_back_image_div_cnt_h1">
           Revolutionize your
           <br />
           Work{" "}
